9749fd7e128aff2446558da2b32dba815649e17b
Jianshen Liu <jliu120@ucsc.edu>
2016-12-20 11:49:46 -0800

    Add two solutions

---
+++ JumpGame.java
@@ -0,0 +1,11 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int last = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] + i >= last) {
+                last = i;
+            }
+        }
+        return last <= 0;
+    }
+}
